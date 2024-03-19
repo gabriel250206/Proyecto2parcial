cee4289d447780dcf4cda0995c65d7e2b005614e
Alberto <19110@portalcedi.edu.mx>
2024-03-19 14:47:38 -0600

    implmentacion menu hasta elegir usuario

--- menu.cpp
+++ menu.cpp
@@ -1,9 +1,64 @@
 #include <bits/stdc++.h>
 #include "rlutil.h"
-#include "usuarios.h"
+
 using namespace std;
 using namespace rlutil;
-vector<Usuario> Usuarios;
+class Usuario
+{
+public:
+    string nombre;
+};
+
+void data()
+{
+    cout << getId;
+}
+void publ(){};
+void ami(){};
+void opciones()
+{
+    int mod = 0;
+    while (1)
+    {
+        system("cls");
+        if (mod > 3)
+            mod = 0;
+        if (mod < 0)
+            mod = 3;
+        if (mod == 0)
+            cout << ">";
+        cout << "datos\n";
+        if (mod == 1)
+            cout << ">";
+        cout << "publicaciones\n";
+        if (mod == 2)
+            cout << ">";
+        cout << "amigos\n";
+        if (mod == 3)
+            cout << ">";
+        cout << "volver\n";
+        if (kbhit)
+        {
+            int k = getkey();
+            if (k == 14)
+                mod--;
+            if (k == 15)
+                mod++;
+            if (k == 1)
+            {
+                if (mod == 0)
+                    data();
+                if (mod == 1)
+                    publ();
+                if (mod == 2)
+                    ami();
+                if (mod == 3)
+                    return;
+            }
+        }
+    }
+}
+
 void Tittle()
 {
     cout << "titulo : ) \n";
@@ -25,8 +80,11 @@ bool menuk()
         {
             if (mod == i)
                 cout << ">";
-            cout << Usuarios[i].nombre;
+            cout << Usuarios[i].nombre << '\n';
         }
+        if (mod == Usuarios.size())
+            cout << ">";
+        cout << "Salir";
         if (kbhit)
         {
             int k = getkey();
@@ -36,6 +94,10 @@ bool menuk()
                 mod++;
             if (k == 1)
             {
+                if (mod == Usuarios.size())
+                    return 0;
+                Usuario a = &Usuarios[mod];
+                else opciones();
             }
         }
     }
